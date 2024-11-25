b08e465597dafb43e2f796aaefd9b0457736bf71
Top Petchsuksan <bhijak@magicboxsolution.com>
2024-11-25 21:05:14 +0700

    Initial commit

---
+++ src/main/kotlin/com/lamoonp/digital_wallet_demo/DigitalWalletDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.lamoonp.digital_wallet_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DigitalWalletDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DigitalWalletDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/lamoonp/digital_wallet_demo/DigitalWalletDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lamoonp.digital_wallet_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DigitalWalletDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
