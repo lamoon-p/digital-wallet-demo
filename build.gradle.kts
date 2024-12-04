25ac66b4f662d737d7dd3496a8113521b1fae54e
Top Petchsuksan <bhijak@magicboxsolution.com>
2024-12-04 19:51:05 +0700

    Add 3 test dependencies into build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.testcontainers:testcontainers:1.19.3")
+	testImplementation("org.testcontainers:mongodb:1.19.3")
+	testImplementation("org.testcontainers:junit-jupiter:1.19.3")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
