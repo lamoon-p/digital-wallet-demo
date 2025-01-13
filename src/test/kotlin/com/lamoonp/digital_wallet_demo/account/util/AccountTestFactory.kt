da371b853a061e70631e543844f9042db6dd0e96
Top Petchsuksan <bhijak@magicboxsolution.com>
2025-01-13 14:29:53 +0700

    Refactor the package name from account to accountservice.
    Change the default balance of the account creation to 100.00

--- src/test/kotlin/com/lamoonp/digital_wallet_demo/account/util/AccountTestFactory.kt
+++ src/test/kotlin/com/lamoonp/digital_wallet_demo/account/util/AccountTestFactory.kt
@@ -1,13 +1,13 @@
 package com.lamoonp.digital_wallet_demo.account.util
 
-import com.lamoonp.digital_wallet_demo.account.model.Account
-import com.lamoonp.digital_wallet_demo.account.model.AccountStatus
+import com.lamoonp.digital_wallet_demo.accountservice.model.Account
+import com.lamoonp.digital_wallet_demo.accountservice.model.AccountStatus
 import java.math.BigDecimal
 
 object AccountTestFactory {
     fun createAccount(
         userId: String = "test-user",
-        balance: BigDecimal = BigDecimal(10000.00),
+        balance: BigDecimal = BigDecimal(100.00),
         currency: String = "THB",
         status: AccountStatus = AccountStatus.ACTIVE
     ) = Account(
