75b8839fe62d5d6713d0c579087ca7b39d711a10
lamoonp <bhijak@gmail.com>
2025-04-22 11:45:16 +0700

    feat(account service): add createAccount add validateCurrency and validateCreateAccountRequest
    Passed 3 unit test

--- src/main/kotlin/com/lamoonp/digital_wallet_demo/accountservice/service/AccountServiceImpl.kt
+++ src/main/kotlin/com/lamoonp/digital_wallet_demo/accountservice/service/AccountServiceImpl.kt
@@ -2,6 +2,7 @@ package com.lamoonp.digital_wallet_demo.accountservice.service
 
 import com.lamoonp.digital_wallet_demo.accountservice.exception.InvalidAccountDataException
 import com.lamoonp.digital_wallet_demo.accountservice.model.Account
+import com.lamoonp.digital_wallet_demo.accountservice.model.AccountStatus
 import com.lamoonp.digital_wallet_demo.accountservice.repository.AccountRepository
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -12,10 +13,60 @@ class AccountServiceImpl(
     private val accountRepository: AccountRepository
 ): AccountService {
 
-    private val
+    private val supportedCurrencies = setOf(
+        Currency.getInstance("USD"),
+        Currency.getInstance("THB"),
+        Currency.getInstance("SGD"),
+    )
+
+    private fun validateCurrency(currencyCode: String): Mono<Currency> {
+        return try {
+            val currency = Currency.getInstance(currencyCode.uppercase())
+            if (currency in supportedCurrencies) {
+                Mono.just(currency)
+            } else {
+                Mono.error(
+                    InvalidAccountDataException(
+                        "Currency $currencyCode is not supported. Supported currencies are: " +
+                                supportedCurrencies.joinToString { it.currencyCode }
+                    )
+                )
+            }
+        } catch (e: IllegalArgumentException) {
+            Mono.error(
+                InvalidAccountDataException("Invalid currency code: $currencyCode")
+            )
+        }
+    }
 
     override fun createAccount(userId: String, currency: String): Mono<Account> {
-        TODO("Not yet implemented")
+        return validateCreateAccountRequest(userId, currency)
+            .flatMap { validCurrency ->
+                accountRepository.save(
+                Account(
+                    accountId = UUID.randomUUID(),
+                    userId = userId,
+                    balance = BigDecimal.ZERO,
+                    currency = validCurrency.currencyCode,
+                    status = AccountStatus.ACTIVE,
+                )
+            )
+        }
+    }
+
+    private fun validateCreateAccountRequest(
+        userId: String,
+        currency: String
+    ): Mono<Currency> {
+        return when {
+            userId.isBlank() -> {
+                Mono.error(
+                    InvalidAccountDataException("User ID cannot be empty")
+                )
+            }
+
+            else -> validateCurrency(currency)
+        }
     }
 
     override fun getAccount(accountId: UUID): Mono<Account> {
@@ -32,18 +83,5 @@ class AccountServiceImpl(
 
     override fun deactivateAccount(accountId: UUID): Mono<Account> {
         TODO("Not yet implemented")
-    }
-
-    private fun validateCreateAccountRequest(
-        userId: String,
-        currency: String
-    ): Mono<Boolean> {
-        return when {
-            userId.isBlank() -> {
-                Mono.error(
-                    InvalidAccountDataException("User ID cannot be empty")
-                )
-            }
-        }
     }
 }
